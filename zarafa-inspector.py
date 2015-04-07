7201666baaa14344a809e2e828d4b14efcb6ef52
Jelle van der Waa <jelle@vdwaa.nl>
2015-04-07 22:47:54 +0200

    WIP: foldertree widget added

--- zarafa-inspector.py
+++ zarafa-inspector.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import sys
 
-from PyQt5.QtCore import QCoreApplication
+from PyQt5.QtCore import QCoreApplication, Qt
 from PyQt5.QtWidgets import *
 from PyQt5 import QtGui
 
@@ -12,6 +12,35 @@ import MAPI.Util.AddressBook
 import MAPI.Tags
 import _MAPICore
 
+# FIXME: move to seperate file
+class FolderTree(QTreeWidget):
+    def __init__(self, parent = None, user = None):
+        super(FolderTree, self).__init__(parent)
+
+        self.header().setHidden(True)
+        self.folders = user.store.folders()
+
+        rootnode = QTreeWidgetItem(self, [user.name])
+        rootnode.setData(0, Qt.UserRole, user.store.root)
+        self.parent = rootnode
+        self.expandItem(rootnode)
+
+        # Draw folder hierachy
+        folders = {}
+        for folder in user.store.folders(system=True, recurse=True): # XXX: configureable?
+            # FIXME: performance / cleaner method without temp dict
+            if folder.depth != 0:
+                parentid = folder.parent.entryid
+                if folders[parentid]:
+                    parent = folders[parentid]
+            else:
+                parent = self.parent
+
+            item = QTreeWidgetItem(parent, [folder.name])
+            item.setData(0, Qt.UserRole, folder)
+            folders[folder.entryid] = item
+
+# FIXME: move to seperate file
 class LoginDialog(QDialog):
     def __init__(self, parent = None):
         super(LoginDialog, self).__init__(parent)
@@ -61,6 +90,26 @@ class ZarafaInspector(QMainWindow):
         file_menu = menubar.addMenu('&file')
         file_menu.addAction(close_action)
 
+        # QtabWidget
+        # TAB1: GAB => show user details and maybe GAB?
+        # TAB2: FolderTree | QListView | QTableWidget
+
+        # FIXME in seperate widget?
+        self.tabwidget = QTabWidget()
+        self.setCentralWidget(self.tabwidget)
+        self.tab1 = QWidget()
+
+        # User Store
+        self.tab2 = QWidget()
+        vbox_layout = QVBoxLayout()
+        vbox_layout.addWidget(FolderTree(user = server.user(server.options.auth_user)))
+        self.tab2.setLayout(vbox_layout)
+        self.tabwidget.addTab(self.tab1, "GAB")
+        self.tabwidget.addTab(self.tab2, "User Store")
+
+        # FIXME: If not SYSTEM
+        self.tabwidget.setCurrentIndex(1)
+
         self.resize(1024, 768)
         self.setWindowTitle('Zarafa Inspector')
         self.show()
@@ -72,7 +121,7 @@ if __name__ == '__main__':
     # Start the app up
     app = QApplication(sys.argv)
 
-    if options.auth_user and options.auth_pass:
+    if options.auth_user and options.auth_pass: # TODO: SYSTEM user?
         server = zarafa.Server(options) # Throw exception if it fails
         inspector = ZarafaInspector(server) # Refactor in one if?
     else:
