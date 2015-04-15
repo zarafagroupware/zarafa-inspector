48fe0eeeebc7a5c180d0277b4911ba8e45239df9
Jelle van der Waa <jelle@vdwaa.nl>
2015-04-15 21:58:10 +0200

    Add contextmenu to TreeWidget, add create/remove folder

--- widgets/Foldertree.py
+++ widgets/Foldertree.py
@@ -1,5 +1,5 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QTreeWidget, QTreeWidgetItem, QHeaderView
+from PyQt5.QtWidgets import QTreeWidget, QTreeWidgetItem, QMenu, QWidget, QAction, QInputDialog
 
 
 # FIXME: move to seperate file
@@ -9,11 +9,15 @@ class FolderTree(QTreeWidget): # QTreeView?
 
         self.header().setHidden(True)
         self.folders = user.store.folders()
+        self.store = user.store
 
         rootnode = QTreeWidgetItem(self, [user.name])
         rootnode.setData(0, Qt.UserRole, user.store.root)
         self.parent = rootnode
         self.expandItem(rootnode)
+
+        self.setContextMenuPolicy(Qt.CustomContextMenu)
+        self.customContextMenuRequested.connect(self.onCustomContextMenu)
 
         # Draw folder hierachy
         folders = {}
@@ -29,3 +33,30 @@ class FolderTree(QTreeWidget): # QTreeView?
             item = QTreeWidgetItem(parent, [folder.name])
             item.setData(0, Qt.UserRole, folder)
             folders[folder.entryid] = item
+
+    def onCustomContextMenu(self, point):
+        menu = QMenu("Menu", self)
+        menu.addAction("Delete folder", self.deleteFolder)
+        menu.addAction("Create folder", self.createFolder)
+
+        menu.exec_(self.mapToGlobal(point))
+
+    def deleteFolder(self):
+        current = self.currentItem()
+        parent = current.parent()
+        folder = current.data(0, Qt.UserRole)
+        folder.parent.delete(folder)
+        parent.removeChild(current)
+        current = None
+
+    def createFolder(self):
+        current = self.currentItem()
+        folder = current.data(0, Qt.UserRole)
+        foldername, ok = QInputDialog.getText(self, 'Create folder Dialog', 'New folder name:')
+        if ok and foldername:
+            newfolder = folder.create_folder(str(foldername)) # TODO: cast to str really needed?
+            item = QTreeWidgetItem(current, [foldername])
+            item.setData(0, Qt.UserRole, newfolder)
+            self.insertTopLevelItem(0, item)
+
+
