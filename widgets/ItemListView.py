07b17a6079451d5447874bcd679cee94537c98a3
Jelle van der Waa <jelle@vdwaa.nl>
2015-04-15 22:10:22 +0200

    Implement contextmenu with delete/save as eml

--- widgets/ItemListView.py
+++ widgets/ItemListView.py
@@ -1,5 +1,5 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QListView
+from PyQt5.QtWidgets import QListView, QFileDialog, QMenu
 
 class ItemListView(QListView):
     '''
@@ -10,4 +10,44 @@ class ItemListView(QListView):
 
     def __init__(self, parent=None):
         super(ItemListView, self).__init__(parent)
-        self.parent = self.parent()
+
+        self.setContextMenuPolicy(Qt.CustomContextMenu)
+        self.customContextMenuRequested.connect(self.onCustomContextMenu)
+
+    def onCustomContextMenu(self, point):
+        index = self.indexAt(point)
+        record = self.model().data(index, Qt.ItemDataRole)
+
+        menu = QMenu("Menu", self)
+
+        if record.message_class.startswith('IPM.Note'):
+            menu.addAction("Save as EML", self.saveEML)
+
+        menu.addAction("Delete Item", self.deleteItem)
+
+        '''
+        if record.attachments():
+            menu.addAction("View attachments", self.showAttachments)
+
+        menu.addAction("View recipients", self.showRecipients)
+        '''
+
+        # Show the context menu.
+        menu.exec_(self.mapToGlobal(point))
+
+    def saveEML(self):
+        current = self.currentIndex()
+        record = self.model().data(current, Qt.ItemDataRole)
+        filename = QFileDialog.getSaveFileName(caption='Save EML', filter='Emails (*.eml)')
+
+        if filename[0]: # TODO: fix test for set entry?
+            rfc882 = record.eml()
+            fname = open(filename[0], 'w') # Check for .eml else append it
+            fname.write(rfc882)
+            fname.close()
+
+    def deleteItem(self):
+        current = self.currentIndex()
+        record = self.model().data(current, Qt.ItemDataRole)
+        record.folder.delete([record])
+        self.model().removeItems([self.currentIndex().row()])
